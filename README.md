c5d5024e4512209302efbff2963a48ee50269db9
tvn201386 <tvn201386@KDTVN-PC0801>
2022-04-22 14:35:47 +0700

    create project structure

---
+++ README.md
@@ -0,0 +1,8 @@
+# ASP.NET Core 3.1 project from TEDU
+## Technologies
+- ASP.NET Core 3.1
+- Entity Framework Core 3.1
+## Install Tools
+- .NET Core SDK 3.1
+- Git client
+- Visual Studio 2019
---
+++ VietWorshipStore.BackendApi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VietWorshipStore.BackendApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
